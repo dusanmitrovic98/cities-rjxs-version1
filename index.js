40e35d8792a4b698a355c5799d27dcb5e0564124
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2020-04-07 08:42:42 +0200

    1 file modified

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ from './src/cities-services.js';
 
 // Write Javascript code!
 
-var citiesObservablee = subscribeFetchCities();
+const citiesObservablee = subscribeFetchCities();
 citiesObservablee.subscribe(cities => {
   console.log(cities);
 })

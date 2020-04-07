d8092e5df5524c759aff9887489a0bb08ba2dada
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2020-04-07 08:30:48 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 import './style.css';
 import { City } from './src/models/City.js';
 import { filterText } from './src/filter-text.js';
+import 
+{
+  subscribeFetchCities
+}
+from './src/cities-services.js';
 
 
 // Write Javascript code!
--- src/cities-services.js
+++ src/cities-services.js
@@ -1,3 +1,14 @@
-const URL_TO_FETCH_CITIES =
-"http://my-json-server.typicode.com/dusanmitrovic98/cities-rjxs-version1";
+import { from } from 'rxjs';
+import { filterText } from './src/filter-text.js';
 
+const URL_TO_FETCH_CITIES =
+"http://my-json-server.typicode.com/dusanmitrovic98/cities-rjxs-version1/cities/";
+
+export function subscribeFetchCities()
+{
+  var citiesObservable = from(
+    fetch(URL_TO_FETCH_CITIES)
+      .then(response => response.json())
+  )
+  return citiesObservable;
+}

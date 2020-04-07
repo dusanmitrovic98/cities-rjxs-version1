18afcae4ea98e1bd64b8be264a648711a493d74c
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2020-04-07 13:01:55 +0200

    1 file modified

--- src/draw-html-elements.js
+++ src/draw-html-elements.js
@@ -14,6 +14,7 @@ export function drawHtmlElements()
     ).subscribe(textToBeFiltered => {
       const filteredText = filterText(textToBeFiltered);
       inputCityName.value = filteredText;
+
       
     })
 
